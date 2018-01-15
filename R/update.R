b3a8b12ebd7da65dfd22d70d010dc867a1e5c95d
Lucas Gray <soulcatalyst@gmail.com>
2018-01-15 12:36:17 -0800

    fixed update_iterclust()

--- R/update.R
+++ R/update.R
@@ -1,3 +1,5 @@
+# Delete before releasing to public
 update_iterclust <- function() {
-  devtools::install_github("https://github.com/AllenInstitute/iterclust")
+  devtools::install_github("AllenInstitute/iterclust",
+                           auth_token = "96cb6605b9e7d395b5b3e3e3c04f0eb001cf4674")
 }
