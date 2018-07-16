4637b98322810faf0e342c14687a52d3ed028ba9
Lucas Graybuck <lucasg@alleninstitute.org>
2018-07-15 21:18:15 -0700

    swap order of normalize functions since lm_normalize() calls lm_matrix()

--- R/normalize.R
+++ R/normalize.R
@@ -1,3 +1,32 @@
+lm_matrix <- function(dat, x)
+{
+  coef <- colSums(t(dat) * x) / sum(x^2)
+  
+  fit <- matrix(coef, ncol=1) %*% matrix(x, nrow = 1)
+  
+  resid <- dat - fit
+  
+  se_r <- rowVars(dat)
+  se_f <- rowVars(resid)
+  
+  R_2 <- 1 - se_f / se_r
+  R_2[is.na(R_2)] <- 0
+  R_2 <- setNames(R_2, row.names(dat))
+  
+  n <- ncol(dat)
+  
+  F_stats <- (se_r - se_f) / (se_f / (n - 2))
+  F_stats <- setNames(F_stats, row.names(dat))
+  
+  p.value <- df(F_stats, 1, n - 2)
+  p.value[is.na(p.value)] <- 1
+  padj <- p.adjust(p.value)
+  
+  fit <- data.frame(coef, R_2, F_stats, p.value, padj)
+  
+  return(list(fit = fit, resid = resid))
+}
+
 #####Add batch substracted median
 lm_normalize <- function(dat, 
                          x, 
@@ -43,31 +72,3 @@ lm_normalize <- function(dat,
   
   }
 
-lm_matrix <- function(dat, x)
-{
-  coef <- colSums(t(dat) * x) / sum(x^2)
-  
-  fit <- matrix(coef, ncol=1) %*% matrix(x, nrow = 1)
-  
-  resid <- dat - fit
-  
-  se_r <- rowVars(dat)
-  se_f <- rowVars(resid)
-  
-  R_2 <- 1 - se_f / se_r
-  R_2[is.na(R_2)] <- 0
-  R_2 <- setNames(R_2, row.names(dat))
-  
-  n <- ncol(dat)
-  
-  F_stats <- (se_r - se_f) / (se_f / (n - 2))
-  F_stats <- setNames(F_stats, row.names(dat))
-  
-  p.value <- df(F_stats, 1, n - 2)
-  p.value[is.na(p.value)] <- 1
-  padj <- p.adjust(p.value)
-  
-  fit <- data.frame(coef, R_2, F_stats, p.value, padj)
-  
-  return(list(fit = fit, resid = resid))
-}
