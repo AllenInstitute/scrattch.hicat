cfdd7c4f2a48d5076c7fbd0afb63ef76249c30ca
Lucas Graybuck <lucasg@alleninstitute.org>
2019-04-01 13:44:31 -0700

    linting and renaming ahead of tests

--- R/util.R
+++ R/util.R
@@ -6,20 +6,21 @@
 #' 
 #' @return a vector containing the numeric position at [rows,cols]
 #' 
-get_pair_matrix_coor <- function(m, rows, cols)
-  {
-    if(!is.numeric(rows)){
-      rows <- match(rows, row.names(m))
-    }
-  
-    if(!is.numeric(cols)){
-      cols <- match(cols, colnames(m))
-    }
-  
-    coor <- (cols - 1) * nrow(m) + rows
-
-    return(coor)
+get_pair_matrix_coor <- function(m, 
+                                 rows, 
+                                 cols) {
+  if(!is.numeric(rows)){
+    rows <- match(rows, row.names(m))
   }
+  
+  if(!is.numeric(cols)){
+    cols <- match(cols, colnames(m))
+  }
+  
+  coor <- (cols - 1) * nrow(m) + rows
+  
+  return(coor)
+}
 
 #' Subset a matrix as a vector using row and column positions
 #' 
@@ -29,12 +30,13 @@ get_pair_matrix_coor <- function(m, rows, cols)
 #' 
 #' @return a vector with values extracted from m at [rows/cols]
 #' 
-get_pair_matrix <- function(m, rows, cols)
-  {
-    v <- as.vector(m)
-    coor <- get_pair_matrix_coor(m, rows, cols)
-    return(v[coor])
-  }
+get_pair_matrix <- function(m, 
+                            rows, 
+                            cols) {
+  v <- as.vector(m)
+  coor <- get_pair_matrix_coor(m, rows, cols)
+  return(v[coor])
+}
 
 
 #' Update a matrix with values from a 1d vector using row and column positions
@@ -46,21 +48,25 @@ get_pair_matrix <- function(m, rows, cols)
 #' 
 #' @return a matrix with updated values
 #' 
-set_pair_matrix <- function(m, rows, cols, vals)
-  {
-    coor <- get_pair_matrix_coor(m, rows, cols)
-    m[coor] <- vals
-    return(m)
-  }
+set_pair_matrix <- function(m, 
+                            rows, 
+                            cols, 
+                            vals) {
+  coor <- get_pair_matrix_coor(m, rows, cols)
+  m[coor] <- vals
+  return(m)
+}
 
 
-get_pairs <- function(pairs.str)
-  {
-    pairs=as.data.frame(do.call("rbind",strsplit(pairs.str, "_")),stringsAsFactors=FALSE)
-    row.names(pairs)=pairs.str
-    colnames(pairs) = c("P1", "P2")
-    return(pairs)
-  }
+get_pairs <- function(pairs.str) {
+  pairs <- as.data.frame(do.call("rbind", strsplit(pairs.str, "_")), 
+                         stringsAsFactors = FALSE)
+  
+  row.names(pairs) <- pairs.str
+  colnames(pairs) <- c("P1", "P2")
+  
+  return(pairs)
+}
 
 
 #' Convert paired cluster comparison values to a matrix
@@ -86,27 +92,24 @@ get_pairs <- function(pairs.str)
 #' 
 convert_pair_matrix <- function(pair.num, 
                                 l = NULL,
-                                directed = FALSE)
-  {
-    pairs <- get_pairs(names(pair.num))
-    
-    if(is.null(l)){
-      l <- sort(unique(c(pairs[,1], pairs[,2])))
-    }
-
-    n.cl <- length(l)
-    
-    pair.num.mat <- matrix(0, nrow = n.cl, ncol = n.cl)
-    rownames(pair.num.mat) <- l
-    colnames(pair.num.mat) <- l
-    pair.num.mat <- set_pair_matrix(pair.num.mat, pairs[,1], pairs[,2], pair.num)
-    if(!directed) {
-      pair.num.mat <- set_pair_matrix(pair.num.mat, pairs[,2], pairs[,1], pair.num)
-    }
-    return(pair.num.mat)
+                                directed = FALSE) {
+  pairs <- get_pairs(names(pair.num))
+  
+  if(is.null(l)){
+    l <- sort(unique(c(pairs[,1], pairs[,2])))
   }
-
-
+  
+  n.cl <- length(l)
+  
+  pair.num.mat <- matrix(0, nrow = n.cl, ncol = n.cl)
+  rownames(pair.num.mat) <- l
+  colnames(pair.num.mat) <- l
+  pair.num.mat <- set_pair_matrix(pair.num.mat, pairs[,1], pairs[,2], pair.num)
+  if(!directed) {
+    pair.num.mat <- set_pair_matrix(pair.num.mat, pairs[,2], pairs[,1], pair.num)
+  }
+  return(pair.num.mat)
+}
 
 #' Convert paired cluster comparison values to a matrix
 #' 
@@ -130,29 +133,29 @@ convert_pair_matrix <- function(pair.num,
 #' pair_matrix
 #' 
 convert_pair_matrix_str <- function(pair.str, 
-                                l = NULL,
-                                directed = FALSE)
-  {
-    pairs <- do.call("rbind", strsplit(names(pair.str),"_"))
-    
-    if(is.null(l)){
-      l <- sort(unique(as.vector(pairs)))
-    }
-    
-    n.cl <- length(l)
-    
-    pair.str.mat <- matrix("", nrow = n.cl, ncol = n.cl)
-    rownames(pair.str.mat) <- l
-    colnames(pair.str.mat) <- l
-    
-    for(i in 1:nrow(pairs)){
-      pair.str.mat[pairs[i,1], pairs[i,2]] <- pair.str[i]
-      if(!directed) {
-        pair.str.mat[pairs[i,2], pairs[i,1]] <- pair.str[i]
-      }
-    }
-    return(pair.str.mat)
+                                    l = NULL,
+                                    directed = FALSE) {
+  
+  pairs <- do.call("rbind", strsplit(names(pair.str),"_"))
+  
+  if(is.null(l)){
+    l <- sort(unique(as.vector(pairs)))
   }
+  
+  n.cl <- length(l)
+  
+  pair.str.mat <- matrix("", nrow = n.cl, ncol = n.cl)
+  rownames(pair.str.mat) <- l
+  colnames(pair.str.mat) <- l
+  
+  for(i in 1:nrow(pairs)){
+    pair.str.mat[pairs[i,1], pairs[i,2]] <- pair.str[i]
+    if(!directed) {
+      pair.str.mat[pairs[i,2], pairs[i,1]] <- pair.str[i]
+    }
+  }
+  return(pair.str.mat)
+}
 
 
 #' Generate a sparse matrix one-hot representation of clusters x samples
@@ -162,23 +165,21 @@ convert_pair_matrix_str <- function(pair.str,
 #' @return a sparse, one-hot matrix indicating which cluster(columns) each sample (rows) belongs to.
 #' @export
 #' 
-get_cl_mat <- function(cl)
-  {
-    library(Matrix)
+get_cl_mat <- function(cl) {
   
-    if(!is.factor(cl)){
-      cl <- as.factor(cl)
-    }
-    cl = droplevels(cl)
-    cl.mat <- sparseMatrix(i = 1:length(cl),  
-                           j = as.integer(cl), 
-                           x = 1)
-    
-    rownames(cl.mat) <- names(cl)
-    colnames(cl.mat) <- levels(cl)
-    
-    return(cl.mat)
+  if(!is.factor(cl)){
+    cl <- as.factor(cl)
   }
+  cl = droplevels(cl)
+  cl.mat <- Matrix::sparseMatrix(i = 1:length(cl),  
+                                 j = as.integer(cl), 
+                                 x = 1)
+  
+  rownames(cl.mat) <- names(cl)
+  colnames(cl.mat) <- levels(cl)
+  
+  return(cl.mat)
+}
 
 #' Compute cluster sums for each row in a matrix
 #' 
@@ -188,18 +189,17 @@ get_cl_mat <- function(cl)
 #' @return a matrix of genes (rows) x clusters (columns) with sums for each cluster
 #' @export
 #' 
-get_cl_sums <- function(mat, cl)
-  {
-    library(Matrix)
+get_cl_sums <- function(mat, 
+                        cl) {
   
-    cl.mat <- get_cl_mat(cl)
-    
-    cl.sums <- Matrix::tcrossprod(mat[,rownames(cl.mat)], Matrix::t(cl.mat))
-    
-    cl.sums <- as.matrix(cl.sums)
-    
-    return(cl.sums)
-  }
+  cl.mat <- get_cl_mat(cl)
+  
+  cl.sums <- Matrix::tcrossprod(mat[,rownames(cl.mat)], Matrix::t(cl.mat))
+  
+  cl.sums <- as.matrix(cl.sums)
+  
+  return(cl.sums)
+}
 
 #' Compute cluster means for each row in a matrix
 #' 
@@ -209,16 +209,16 @@ get_cl_sums <- function(mat, cl)
 #' @return a matrix of genes (rows) x clusters (columns) with means for each cluster
 #' @export
 #' 
-get_cl_means <- function(mat, cl)
-  {
-
-    cl.sums <- get_cl_sums(mat, cl)
-    
-    cl.size <- table(cl)
-    
-    cl.means <- as.matrix(Matrix::t(Matrix::t(cl.sums)/as.vector(cl.size[colnames(cl.sums)])))
-    
-    return(cl.means)
+get_cl_means <- function(mat, 
+                         cl) {
+  
+  cl.sums <- get_cl_sums(mat, cl)
+  
+  cl.size <- table(cl)
+  
+  cl.means <- as.matrix(Matrix::t(Matrix::t(cl.sums)/as.vector(cl.size[colnames(cl.sums)])))
+  
+  return(cl.means)
 }
 
 #' Compute cluster medians for each row in a matrix
@@ -238,7 +238,7 @@ get_cl_medians <- function(mat, cl)
                     tapply(names(cl), 
                            cl, 
                            function(x){
-                             rowMedians(as.matrix(mat[,x]))
+                             matrixStats::rowMedians(as.matrix(mat[,x]))
                            }
                     )
   )
@@ -278,8 +278,8 @@ get_cl_prop <- function(mat,
 #' 
 #' @return a matrix of correlation values between each column of m
 #'  
-sparse_cor <- function(m){
-  library(Matrix)
+sparse_cor <- function(m) {
+  #library(Matrix)
   
   n_rows <- nrow(m)
   n_cols <- ncol(m)
@@ -291,15 +291,15 @@ sparse_cor <- function(m){
   nozero <- as.vector(m[ii,]) - rep(Ex, each = length(ii))        # colmeans
   
   covmat <- (crossprod(matrix(nozero, ncol = n_cols)) +
-             crossprod(t(Ex)) * (n_rows - length(ii))
-             ) / (n_rows - 1)
+               crossprod(t(Ex)) * (n_rows - length(ii))
+  ) / (n_rows - 1)
   
   sdvec <- sqrt(diag(covmat))
   
   cormat <- covmat / crossprod(t(sdvec))
-
+  
   return(cormat)
-  }
+}
 
 #' Calculate Tau scores for each gene
 #' 
@@ -308,12 +308,12 @@ sparse_cor <- function(m){
 #' 
 #' @return a vector of Tau scores
 #' 
-calc_tau <- function(m, byRow = TRUE)
-{
+calc_tau <- function(m, 
+                     byRow = TRUE) {
   if(!byRow){
     m <- t(m)
   }
-  m <- m / rowMaxs(m)
+  m <- m / matrixStats::rowMaxs(m)
   tau <- rowSums(1 - m) / (ncol(m) - 1)
   tau[is.na(tau)] <- 0
   return(tau)
@@ -326,67 +326,74 @@ calc_tau <- function(m, byRow = TRUE)
 #' 
 #' @return A cluster factor object containing sampled cells
 #' 
-sample_cells<- function(cl, sample.size, weights = NULL)
-{
+sample_cells<- function(cl, 
+                        sample.size, 
+                        weights = NULL) {
   
   n_cl <- unique(cl)
   
   if(length(sample.size) == 1) {
     
     sample.size <- setNames(rep(sample.size, length(n_cl)), n_cl)
-  
+    
   }
   
   cl.cells <- split(names(cl), cl)
   
   sampled.cells <- unlist(sapply(names(cl.cells), 
                                  function(x){
-    cells <- cl.cells[[x]]
-    if(sample.size[[x]] == length(cells)){
-      return(cells)
-    }
-    
-    to.sample <- pmin(sample.size[[x]], length(cells))
-    
-    if(!is.null(weights)){
-      sampled <- sample(cells, to.sample, prob = weights[cells])
-    }
-    
-    else{
-      sampled <- sample(cells, to.sample)
-    }
-    
-    sampled
-    
-  }, simplify = FALSE))
+                                   cells <- cl.cells[[x]]
+                                   if(sample.size[[x]] == length(cells)){
+                                     return(cells)
+                                   }
+                                   
+                                   to.sample <- pmin(sample.size[[x]], length(cells))
+                                   
+                                   if(!is.null(weights)){
+                                     sampled <- sample(cells, to.sample, prob = weights[cells])
+                                   }
+                                   
+                                   else{
+                                     sampled <- sample(cells, to.sample)
+                                   }
+                                   
+                                   sampled
+                                   
+                                 }, simplify = FALSE))
   
   return(sampled.cells)
 }
 
-
-cpm <- function(counts)
-  {
-    library(Matrix)
-    sf = Matrix::colSums(counts)/10^6
-    if(is.matrix(counts)){    
-      return(t(t(counts) /sf))
-    }
-    else if(class(counts)=="dgCMatrix"){
-      library(IRanges)                  
-      sep = counts@p
-      sep = sep[-1] - sep[-length(sep)]
-      j = Rle(1:length(sep), sep)
-      counts@x = counts@x/sf[as.integer(j)]
-    }
-    else if(class(counts)=="dgTMatrix"){
-      j = counts@j
-      counts@x = counts@x/sf[j+1]
-    }
-    else{
-      stop(paste("cpm function for", class(counts)[1], "not supported"))
-    }
-    return(counts)
+#' Convert a matrix of raw counts to a matrix of Counts per Million values
+#' 
+#' The input can be a base R matrix or a sparse matrix from the Matrix package.
+#' 
+#' This function expects that columns correspond to samples, and rows to genes.
+#' 
+#' @param counts a matrix of count values.
+cpm <- function(counts) {
+  
+  library(Matrix)
+  
+  sf <- Matrix::colSums(counts) / 1e6
+  
+  if(is.matrix(counts)){    
+    return(t(t(counts) / sf))
+  } else if(class(counts) == "dgCMatrix") {
+    sep <- counts@p
+    sep <- sep[-1] - sep[-length(sep)]
+    j <- S4Vectors::Rle(1:length(sep), sep)
+    counts@x <- counts@x / sf[as.integer(j)]
   }
+  else if(class(counts)=="dgTMatrix"){
+    j = counts@j
+    counts@x = counts@x/sf[j+1]
+  }
+  else{
+    stop(paste("cpm function for", class(counts)[1], "not supported"))
+  }
+  return(counts)
+}
 
 logCPM <- function(counts)
   {
