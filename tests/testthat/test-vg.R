890a5a5b2f9ce29d06d2025be52be7b1828c426e
Lucas Graybuck <lucasg@alleninstitute.org>
2019-04-03 17:59:37 -0700

    split findVG(); starting tests for new compute_vg().

--- tests/testthat/test-vg.R
+++ tests/testthat/test-vg.R
@@ -1,5 +1,68 @@
 context("test-vg")
 library(scrattch.hicat)
 
-## findVG() tests
+# Load glial test data
+library(tasic2016data)
 
+glial_classes <- c("Astrocyte", "Endothelial Cell", "Microglia", 
+                   "Oligodendrocyte", "Oligodendrocyte Precursor Cell")
+glial_cells <- tasic_2016_anno[tasic_2016_anno$broad_type %in% glial_classes, ]
+glial_cells <- glial_cells[glial_cells$secondary_type_id == 0, ]
+
+glial_data <- log2(tasic_2016_counts[, glial_cells$sample_name] + 1)
+
+## compute_vg() tests
+
+test_that(
+  "compute_vg() calculates gene variance statistics.",
+  {
+    vg_results <- compute_vg(glial_data)
+    
+    expect_is(vg_results, "data.frame")
+    
+    sparse_data <- as(glial_data, "dgCMatrix")
+    
+    sparse_vg_results <- compute_vg(sparse_data)
+    
+    expect_is(sparse_vg_results, "data.frame")
+    
+    expect_equal(sparse_vg_results, vg_results)
+    
+    ## Spot Checks
+    check_genes <- c("Opalin", "Hspa8", "Mbp")
+    glial_sample_totals <- colSums(glial_data)
+    glial_scaling <- glial_sample_totals / median(glial_sample_totals)
+    glial_data_scaled <- glial_data / glial_scaling[col(glial_data)]
+    
+    check_means <- rowMeans(glial_data_scaled[check_genes,])
+    names(check_means) <- NULL
+    expect_equal(vg_results$g.means[match(check_genes, vg_results$gene)],
+                 check_means)
+    
+    check_vars <- apply(glial_data_scaled[check_genes,], 1, 
+                        function(x) {
+                          # correct for differences in var calculation
+                          # var() uses the N-1 denominator version; we use N.
+                          var(x) * (length(x) - 1) / length(x) 
+                        })
+    names(check_vars) <- NULL
+    expect_equal(vg_results$g.vars[match(check_genes, vg_results$gene)],
+                 check_vars)
+  }
+)
+
+## plot_vg() tests
+test_that(
+  "plot_vg() generates plots of gene variance statistics.",
+  {
+    
+  }
+)
+
+## findVG() tests
+test_that(
+  "findVG() combines compute_vg() and plot_vg() as necessary.",
+  {
+    
+  }
+)
