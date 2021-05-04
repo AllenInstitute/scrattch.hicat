346c8a4c377fdb85a052b217eb0451ef1fa9d05d
Zizhen Yao <zizheny@alleninstitute.org>
2021-05-04 18:03:05 -0400

    Update RcppExport

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,20 +6,20 @@
 using namespace Rcpp;
 
 // ImputeKnn
-void ImputeKnn(IntegerMatrix knn_idx, IntegerVector cell_idx, IntegerVector ref_idx, NumericMatrix dat, Nullable<IntegerVector> gene_idx_, Nullable<NumericMatrix> w_mat_, NumericMatrix impute_dat, bool transpose_input, bool transpose_output);
-RcppExport SEXP _scrattch_hicat_ImputeKnn(SEXP knn_idxSEXP, SEXP cell_idxSEXP, SEXP ref_idxSEXP, SEXP datSEXP, SEXP gene_idx_SEXP, SEXP w_mat_SEXP, SEXP impute_datSEXP, SEXP transpose_inputSEXP, SEXP transpose_outputSEXP) {
+void ImputeKnn(IntegerMatrix knn_idx, IntegerVector ref_idx, IntegerVector cell_idx, IntegerVector gene_idx, NumericMatrix dat, NumericMatrix impute_dat, Nullable<NumericMatrix> w_mat_, bool transpose_input, bool transpose_output);
+RcppExport SEXP _scrattch_hicat_ImputeKnn(SEXP knn_idxSEXP, SEXP ref_idxSEXP, SEXP cell_idxSEXP, SEXP gene_idxSEXP, SEXP datSEXP, SEXP impute_datSEXP, SEXP w_mat_SEXP, SEXP transpose_inputSEXP, SEXP transpose_outputSEXP) {
 BEGIN_RCPP
     Rcpp::RNGScope rcpp_rngScope_gen;
     Rcpp::traits::input_parameter< IntegerMatrix >::type knn_idx(knn_idxSEXP);
-    Rcpp::traits::input_parameter< IntegerVector >::type cell_idx(cell_idxSEXP);
     Rcpp::traits::input_parameter< IntegerVector >::type ref_idx(ref_idxSEXP);
+    Rcpp::traits::input_parameter< IntegerVector >::type cell_idx(cell_idxSEXP);
+    Rcpp::traits::input_parameter< IntegerVector >::type gene_idx(gene_idxSEXP);
     Rcpp::traits::input_parameter< NumericMatrix >::type dat(datSEXP);
-    Rcpp::traits::input_parameter< Nullable<IntegerVector> >::type gene_idx_(gene_idx_SEXP);
-    Rcpp::traits::input_parameter< Nullable<NumericMatrix> >::type w_mat_(w_mat_SEXP);
     Rcpp::traits::input_parameter< NumericMatrix >::type impute_dat(impute_datSEXP);
+    Rcpp::traits::input_parameter< Nullable<NumericMatrix> >::type w_mat_(w_mat_SEXP);
     Rcpp::traits::input_parameter< bool >::type transpose_input(transpose_inputSEXP);
     Rcpp::traits::input_parameter< bool >::type transpose_output(transpose_outputSEXP);
-    ImputeKnn(knn_idx, cell_idx, ref_idx, dat, gene_idx_, w_mat_, impute_dat, transpose_input, transpose_output);
+    ImputeKnn(knn_idx, ref_idx, cell_idx, gene_idx, dat, impute_dat, w_mat_, transpose_input, transpose_output);
     return R_NilValue;
 END_RCPP
 }
